fbd3232784cba3fa3ad72bc65b1d023692471b23
Zachary Balder <zmbalder@gmail.com>
2020-01-20 23:10:21 -0500

    tor

--- install-tools.sh
+++ install-tools.sh
@@ -8,6 +8,7 @@ apt install -y curl
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 
 apt install -y \
+  apt-transport-https \
   docker.io \
   enigmail \
   git \
@@ -26,5 +27,15 @@ update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# Install Tor
+echo 'deb https://deb.torproject.org/torproject.org bionic main
+deb-src https://deb.torproject.org/torproject.org bionic main' > /etc/apt/sources.list.d/tor.list
+
+curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
+
+apt update
+apt install -y tor deb.torproject.org-keyring
 
 apt autoremove -y
