a2f7679df5df4a3ed41a972d8cf8888b35805707
zbo14 <zbalder@starry.com>
2020-01-28 16:00:48 -0500

    add tldr-pages

--- install-tools.sh
+++ install-tools.sh
@@ -29,3 +29,6 @@ update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# Install tldr-pages
+npm install -g tldr
