c7db10f8a9e103601635c9fe4a991c119127642c
zbo14 <zbalder@starry.com>
2020-01-28 14:08:16 -0500

    add pass and remove tor

--- install-tools.sh
+++ install-tools.sh
@@ -8,19 +8,19 @@ apt install -y curl
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 
 apt install -y \
-  apt-transport-https \
   docker.io \
-  enigmail \
   git \
   net-tools \
   nmap \
   nodejs \
+  pass \
   pylint \
   python3-pip \
   shellcheck \
-  thunderbird \
   tmux \
   vim
+
+apt autoremove -y
 
 # Remove Python2 and make Python3 the default
 update-alternatives --remove python /usr/bin/python2
@@ -29,18 +29,3 @@ update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-# Install Tor
-echo 'deb https://deb.torproject.org/torproject.org bionic main
-deb-src https://deb.torproject.org/torproject.org bionic main' > /etc/apt/sources.list.d/tor.list
-
-curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
-gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
-
-apt update
-apt install -y tor deb.torproject.org-keyring
-
-echo 'DNSPort 53
-SocksPort 9050' > /etc/tor/torrc
-
-apt autoremove -y
