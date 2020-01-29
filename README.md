9cc0a20b3c1fe259a68668e60ef60a9f28d3cf60
Zachary Balder <zmbalder@gmail.com>
2020-01-29 17:44:47 -0500

    don't run entire script as root, add pyenv and nvm stuff

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Install script for stuff I like to use on Ubuntu.
 
 ## Usage
 
-The following command pipes a script to bash with root privileges 😱
+The following command pipes a script to bash 😱
 
 Please look at `./install-tools.sh` so you have some idea what's going on!
 
-`wget -qO- https://raw.githubusercontent.com/zbo14/install-tools/master/install-tools.sh | sudo -E bash -`
+`wget -qO- https://raw.githubusercontent.com/zbo14/install-tools/master/install-tools.sh | bash -`
--- install-tools.sh
+++ install-tools.sh
@@ -1,34 +1,50 @@
 #!/bin/bash -e
 
-apt update
-apt upgrade -y
-apt install -y curl
+sudo apt update
+sudo apt upgrade -y
 
-# Install Node 12.x
-curl -sL https://deb.nodesource.com/setup_12.x | bash -
+sudo apt install -y \
+    curl \
+    docker.io \
+    git \
+    net-tools \
+    nmap \
+    pass \
+    pylint \
+    python3-pip \
+    shellcheck \
+    tmux \
+    vim
 
-apt install -y \
-  docker.io \
-  git \
-  net-tools \
-  nmap \
-  nodejs \
-  pass \
-  pylint \
-  python3-pip \
-  shellcheck \
-  tmux \
-  vim
-
-apt autoremove -y
+sudo apt autoremove -y
 
 # Remove Python2 and make Python3 the default
-update-alternatives --remove python /usr/bin/python2
-update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+sudo update-alternatives --remove python /usr/bin/python2
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+
+# Install pyenv
+rm -rf ~/.pyenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash -
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv update
 
 # Install Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Install nvm
+curl -o-  https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash -
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Install Node 12.x LTS
+nvm i 12.14.1
+
+# Update npm
+npm install -g npm
 
 # Install tldr-pages
 npm install -g tldr
