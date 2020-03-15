e515094a8114a3d0783f538b8e42928a17785bf7
zachary balder <zmbalder@gmail.com>
2020-03-15 13:26:37 -0400

    remove extra space

--- install-tools.sh
+++ install-tools.sh
@@ -39,7 +39,7 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Install nvm
-curl -o-  https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
