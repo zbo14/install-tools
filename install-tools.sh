2120e18e736260ff5bc83ceeb89ba7709854dbac
zach balder <zmbalder@gmail.com>
2021-06-20 16:19:39 -0400

    add ncat, remove yarn since it can be default-npm-package

--- install-tools.sh
+++ install-tools.sh
@@ -12,6 +12,7 @@ sudo apt install -y \
   hping3 \
   jq \
   mutt \
+  ncat \
   net-tools \
   nmap \
   pass \
@@ -47,11 +48,6 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-# Install yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install --no-install-recommends yarn
 
 # Install amass
 sudo snap install amass
