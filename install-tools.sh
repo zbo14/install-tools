a8d2b1a76ff8f2d7e4644fd9aa748d79d2d10f86
zach balder <zmbalder@gmail.com>
2021-06-22 15:27:29 -0400

    asdf plugins, install latest versions of docker compose and asdf

--- install-tools.sh
+++ install-tools.sh
@@ -40,20 +40,41 @@ git clone --quiet https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
-# Install asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+# Install latest release of asdf
+asdf="$(wget https://github.com/asdf-vm/asdf/releases -O- 2>/dev/null)"
+pattern='v[0-9]*\.[0-9]*\.[0-9]*'
+
+[[ "$asdf" =~ $pattern ]]
+
+version="${BASH_REMATCH[0]}"
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch "$version"
+
+# Add language plugins
+~/.asdf/bin/asdf plugin-add deno
+~/.asdf/bin/asdf plugin-add golang
+~/.asdf/bin/asdf plugin-add nodejs
+~/.asdf/bin/asdf plugin-add python
+~/.asdf/bin/asdf plugin-add ruby
+~/.asdf/bin/asdf plugin-add rust
 
 # Remove Python2 and make Python3 the default
 sudo update-alternatives --remove python /usr/bin/python2
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 
-# Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Install latest stable Docker Compose
+docker="$(wget https://docs.docker.com/compose/install/ -O- 2>/dev/null)"
+pattern='https://github.com/docker/compose/releases/download/([0-9]*\.[0-9]*\.[0-9]*)'
+
+[[ "$docker" =~ $pattern ]]
+
+version="${BASH_REMATCH[1]}"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/$version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Install snaps
 sudo snap install amass
-sudo snap install sublime-text
 
 mkdir -p ~/Projects
 cd ~/Projects
